d369336e35956550699dfa0433e6cdc926be9add
accsystem360 <3.20356572e+08+accsystem360@users.noreply.github.com>
2026-08-24 05:45:51 +0000

    fix: remove unused widget test import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,6 @@ import 'package:acsys360/domain/repositories/workspace_repository.dart';
 import 'package:acsys360/main.dart';
 import 'package:acsys360/presentation/state/editor_controller.dart';
 import 'package:acsys360/presentation/widgets/line_numbered_editor.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 class FakeWorkspaceRepository implements WorkspaceRepository {
