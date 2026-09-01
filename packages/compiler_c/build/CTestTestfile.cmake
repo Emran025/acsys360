6f6e1c35abc8a08c8028c7083b4bf54694372fdf
accsystem360 <3.20356572e+08+accsystem360@users.noreply.github.com>
2026-09-01 00:22:51 +0000

    chore(compiler): ignore C build artifacts

--- packages/compiler_c/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/acsys360/packages/compiler_c
-# Build directory: /home/ubuntu/acsys360/packages/compiler_c/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(arabicc_c_lexer_test "/home/ubuntu/acsys360/packages/compiler_c/build/arabicc_c_lexer_test")
-set_tests_properties(arabicc_c_lexer_test PROPERTIES  _BACKTRACE_TRIPLES "/home/ubuntu/acsys360/packages/compiler_c/CMakeLists.txt;20;add_test;/home/ubuntu/acsys360/packages/compiler_c/CMakeLists.txt;0;")
